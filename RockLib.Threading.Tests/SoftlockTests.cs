7e07efd43b9dbd34ae491fd1d39676ba0ff242aa
Jason Bock <jasonbock1@rocketmortgage.com>
2022-01-31 11:49:16 -0600

    Initial updates are good

--- RockLib.Threading.Tests/SoftlockTests.cs
+++ RockLib.Threading.Tests/SoftlockTests.cs
@@ -3,54 +3,54 @@ using Xunit;
 
 namespace RockLib.Threading.Tests
 {
-    public class SoftlockTests
-    {
-        [Fact(DisplayName = "TryAcquire method sets IsLockAckquired property to true")]
-        public void TryAcquireTest1()
-        {
-            var softlock = new SoftLock();
+   public class SoftlockTests
+   {
+      [Fact(DisplayName = "TryAcquire method sets IsLockAckquired property to true")]
+      public void TryAcquireTest1()
+      {
+         var softlock = new SoftLock();
 
-            // Prove that the lock is not acquired initially
-            softlock.IsLockAcquired.Should().BeFalse();
+         // Prove that the lock is not acquired initially
+         softlock.IsLockAcquired.Should().BeFalse();
 
-            // This will set the lock to true
-            softlock.TryAcquire();
+         // This will set the lock to true
+         softlock.TryAcquire();
 
-            softlock.IsLockAcquired.Should().BeTrue();
-        }
+         softlock.IsLockAcquired.Should().BeTrue();
+      }
 
-        [Fact(DisplayName = "TryAcquire returns true when the lock is acquired")]
-        public void TryAcquireTest2()
-        {
-            var softlock = new SoftLock();
+      [Fact(DisplayName = "TryAcquire returns true when the lock is acquired")]
+      public void TryAcquireTest2()
+      {
+         var softlock = new SoftLock();
 
-            softlock.TryAcquire().Should().BeTrue();
-        }
+         softlock.TryAcquire().Should().BeTrue();
+      }
 
-        [Fact(DisplayName = "TryAcquire returns false when the lock has already been acquired")]
-        public void TryAcquireTest3()
-        {
-            var softlock = new SoftLock();
+      [Fact(DisplayName = "TryAcquire returns false when the lock has already been acquired")]
+      public void TryAcquireTest3()
+      {
+         var softlock = new SoftLock();
 
-            softlock.TryAcquire();
+         softlock.TryAcquire();
 
-            softlock.TryAcquire().Should().BeFalse();
-        }
+         softlock.TryAcquire().Should().BeFalse();
+      }
 
-        [Fact(DisplayName = "Release method sets IsLockAcquired property back to false")]
-        public void ReleaseTest()
-        {
-            var softlock = new SoftLock();
+      [Fact(DisplayName = "Release method sets IsLockAcquired property back to false")]
+      public void ReleaseTest()
+      {
+         var softlock = new SoftLock();
 
-            softlock.TryAcquire();
+         softlock.TryAcquire();
 
-            // prove it is locked
-            softlock.IsLockAcquired.Should().BeTrue("If this failed we have issues in TryAcquire");
+         // prove it is locked
+         softlock.IsLockAcquired.Should().BeTrue("If this failed we have issues in TryAcquire");
 
-            softlock.Release();
+         softlock.Release();
 
-            // verify it has been released
-            softlock.IsLockAcquired.Should().BeFalse();
-        }
-    }
+         // verify it has been released
+         softlock.IsLockAcquired.Should().BeFalse();
+      }
+   }
 }
